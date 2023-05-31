b5016ace0542bea5867c4267a221078da4bd9577
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-31 07:37:53 -0400

    Passes test scenario 1, displaying contents

---
+++ Library.js
@@ -0,0 +1,11 @@
+class Library {
+    constructor() {
+        this.contents = [];
+    }
+
+    displayContents() {
+        return this.contents
+    }
+}
+
+module.exports = Library;
--- Library.test.js
+++ Library.test.js
@@ -1,4 +1,21 @@
-// contains Books
+const Library = require('./Library')
+
+describe("The Library", () => {
+    let library;
+
+    beforeEach(() => {
+        library = new Library();
+    })
+
+    // contains Books
+    test("contains books", () => {
+        expect(library.displayContents()).toEqual([])
+    })
+
+
+})
+
+
 // The Library should be able to have Books be added to its inventory.
 // The Library should be able to issue()  (check out to patrons) books
 // The Library should be able to accept books (accept returned books from patrons), as well.
