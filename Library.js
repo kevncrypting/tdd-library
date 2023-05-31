dbab372433509491b4f6bc29fd8f10be688dc2c6
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-31 08:00:56 -0400

    Passes test scenario 3, issuing book to patron

--- Library.js
+++ Library.js
@@ -10,6 +10,25 @@ class Library {
     addBook(book) {
         this.inventory.push(...book);
     }
+
+    issueBook(bookTitle, patron) {
+        let selectedBook = this.inventory.find(
+            (book) => book.title === bookTitle
+        );
+
+        if (selectedBook == null) {
+            return "Book Not Found";
+        }
+
+        if (selectedBook.isCheckedOut === true) {
+            return "Book Not Available";
+        }
+
+        if (selectedBook.isCheckedOut === false && patron !== null) {
+            selectedBook.isCheckedOut = true;
+            selectedBook.possessedBy = patron;
+        }
+    }
 }
 
 module.exports = Library;
--- Library.test.js
+++ Library.test.js
@@ -4,6 +4,27 @@ const Library = require("./Library");
 describe("The Library", () => {
     let library;
 
+    let newInventory = [
+        {
+            title: "Immune",
+            author: "Philipp Dettmer",
+            isCheckedOut: false,
+            possessedBy: "Library",
+        },
+        {
+            title: "Drive",
+            author: "Daniel Pink",
+            isCheckedOut: false,
+            possessedBy: "Library",
+        },
+        {
+            title: "Refactoring UI",
+            author: ["Steve Schoger", "Adam Watham"],
+            isCheckedOut: false,
+            possessedBy: "Library",
+        },
+    ];
+
     beforeEach(() => {
         library = new Library();
     });
@@ -15,14 +36,38 @@ describe("The Library", () => {
 
     // should be able to have books be added to its inventory.
     test("should be able to have Books be added to its inventory", () => {
-        library.addBook([{ title: "Immune", author: "Philipp Dettmer" }]);
+        library.addBook(newInventory);
+
+        expect(library.inventory).toEqual(newInventory);
+    });
+
+    // should be able to issue() (check out to patrons) books
+    test("should be able to issue books to patrons)", () => {
+        library.addBook(newInventory);
+        library.issueBook("Immune", "Kevin");
 
         expect(library.inventory).toEqual([
-            { title: "Immune", author: "Philipp Dettmer" },
+            {
+                title: "Immune",
+                author: "Philipp Dettmer",
+                isCheckedOut: true,
+                possessedBy: "Kevin",
+            },
+            {
+                title: "Drive",
+                author: "Daniel Pink",
+                isCheckedOut: false,
+                possessedBy: "Library",
+            },
+            {
+                title: "Refactoring UI",
+                author: ["Steve Schoger", "Adam Watham"],
+                isCheckedOut: false,
+                possessedBy: "Library",
+            },
         ]);
     });
 });
 
-// The Library should be able to issue()  (check out to patrons) books
 // The Library should be able to accept books (accept returned books from patrons), as well.
 // The Library should be able to display an accurate inventory of its books at any time
