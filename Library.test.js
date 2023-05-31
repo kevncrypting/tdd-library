f12c0f1b1aa832d6d0f6fbc1a8aba2b6a1ef05fe
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-05-31 07:00:04 -0400

    Adds jest test script and test scenarios in comments

---
+++ Library.test.js
@@ -0,0 +1,5 @@
+// contains Books
+// The Library should be able to have Books be added to its inventory.
+// The Library should be able to issue()  (check out to patrons) books
+// The Library should be able to accept books (accept returned books from patrons), as well.
+// The Library should be able to display an accurate inventory of its books at any time
